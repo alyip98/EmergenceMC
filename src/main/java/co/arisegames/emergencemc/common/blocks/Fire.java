afc42439a77aeba9f8022cb64e93bb89e365055e
Alfred <alyip98@gmail.com>
2021-07-07 00:50:52 +0800

    Change to MCP mappings

---
+++ src/main/java/co/arisegames/emergencemc/common/blocks/Fire.java
@@ -0,0 +1,10 @@
+package co.arisegames.emergencemc.common.blocks;
+
+import net.minecraft.block.FireBlock;
+
+public class Fire extends FireBlock {
+
+    public Fire(Properties builder) {
+        super(builder);
+    }
+}
